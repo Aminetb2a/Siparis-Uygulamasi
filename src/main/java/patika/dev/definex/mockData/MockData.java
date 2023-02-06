9e3f5b9bb054331c41004087242c9fba92661e75
aminet2a <aminetbo@gmail.com>
2023-02-06 21:29:09 +0100

    create Mock Data reader class.

---
+++ src/main/java/patika/dev/definex/mockData/MockData.java
@@ -0,0 +1,41 @@
+package patika.dev.definex.mockData;
+
+import com.google.common.reflect.TypeToken;
+import patika.dev.definex.entity.Customer;
+import patika.dev.definex.entity.Invoice;
+
+import java.util.List;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.io.InputStream;
+import com.google.gson.Gson;
+import java.lang.reflect.Type;
+import com.google.common.io.Resources;
+import org.apache.commons.io.IOUtils;
+import patika.dev.definex.entity.Order;
+
+import java.nio.charset.StandardCharsets;
+
+public class MockData {
+
+    public static List<Invoice> getInvoices() throws IOException {
+        InputStream inputStream = Resources.getResource("invoice.json").openStream();
+        String json = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+        Type listType = new TypeToken<ArrayList<Invoice>>() {}.getType();
+        return new Gson().fromJson(json, listType);
+    }
+
+    public static List<Customer> getCustomers() throws IOException {
+        InputStream inputStream = Resources.getResource("customer.json").openStream();
+        String json = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+        Type listType = new TypeToken<ArrayList<Customer>>() {}.getType();
+        return new Gson().fromJson(json, listType);
+    }
+
+    public static List<Order> getOrders() throws IOException {
+        InputStream inputStream = Resources.getResource("order.json").openStream();
+        String json = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+        Type listType = new TypeToken<ArrayList<Order>>() {}.getType();
+        return new Gson().fromJson(json, listType);
+    }
+}
