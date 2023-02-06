2045f78e8dc11d911ea9324054a8c90cc0ff0dc6
aminet2a <aminetbo@gmail.com>
2023-02-06 21:35:00 +0100

    create Customer Service class with initial needed methods.

---
+++ src/main/java/patika/dev/definex/service/CustomerService.java
@@ -0,0 +1,25 @@
+package patika.dev.definex.service;
+
+import patika.dev.definex.entity.Customer;
+
+import java.util.List;
+
+public class CustomerService {
+
+    public List<Customer> getCustomerList() {
+        return null;
+    }
+
+    public void addCustomer(Customer customer) {
+
+    }
+
+    public List<Customer> filterCustomerByName(String subName) {
+        return null;
+    }
+
+    public List<Customer> getCustomerByInsertionMonth(int month) {
+        return null;
+    }
+
+}
