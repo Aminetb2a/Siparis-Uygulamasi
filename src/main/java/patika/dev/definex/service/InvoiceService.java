c89a9c728f2beeabb8cb256a2981c92eda646b72
aminet2a <aminetbo@gmail.com>
2023-02-06 22:07:06 +0100

    add methods implementations to Invoice Service

--- src/main/java/patika/dev/definex/service/InvoiceService.java
+++ src/main/java/patika/dev/definex/service/InvoiceService.java
@@ -1,34 +1,64 @@
 package patika.dev.definex.service;
 
 import patika.dev.definex.entity.Invoice;
+import patika.dev.definex.mockData.MockData;
 
 import java.util.List;
+import java.io.IOException;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.*;
 
 public class InvoiceService {
 
-    public List<Invoice> getAllInvoices() {
-        return null;
+    public List<Invoice> getAllInvoices() throws IOException {
+        return MockData.getInvoices();
     }
 
-    public List<Invoice> getTotalInvoicesByMonth(int month) {
-        return null;
+    public List<Invoice> getTotalInvoicesByMonth(int month) throws IOException {
+        return MockData.getInvoices()
+                .parallelStream()
+                .filter(invoice -> invoice.getTransDate().getMonthValue() == month)
+                .collect(Collectors.toList());
     }
 
-    public List<String> getTotalInvoicesByMonth(int month, double average) {
-        return null;
+    public List<String> getTotalInvoicesByMonth(int month, double average) throws IOException {
+        Map<String, Double> averageGroupBySector = MockData.getInvoices()
+                .parallelStream()
+                .collect(groupingBy(Invoice::getSector,
+                        filtering(invoice -> invoice.getTransDate().getMonthValue() == month,
+                                averagingDouble(Invoice::getTotalAmount))));
+
+        return averageGroupBySector.entrySet()
+                .parallelStream()
+                .filter(sector -> sector.getValue() < average )
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
     }
 
-    public List<Invoice> getInvoicesTotalGreaterThan(double total) {
-        return null;
+    public List<Invoice> getInvoicesTotalGreaterThan(double total) throws IOException {
+        return MockData.getInvoices()
+                .parallelStream()
+                .filter(invoice -> invoice.getTotalAmount() > total)
+                .collect(Collectors.toList());
     }
 
-    public List<String> getInvoicesTotalSmallerThanCustomerName(double total) {
-        return null;
+    public List<String> getInvoicesTotalSmallerThanCustomerName(double total) throws IOException {
+        return MockData.getInvoices()
+                .parallelStream()
+                .filter(invoice -> invoice.getTotalAmount() < total)
+                .map(Invoice::getCustomerName)
+                .collect(Collectors.toList());
     }
 
-    public void getInvoicesAverageOfTotalAmountGreaterThan(double total) {
-
+    public double getInvoicesAverageOfTotalAmountGreaterThan(double total) throws IOException {
+        return MockData.getInvoices()
+                .parallelStream()
+                .filter(invoice -> invoice.getTotalAmount() > total)
+                .mapToDouble(Invoice::getTotalAmount)
+                .average()
+                .getAsDouble();
     }
 
 }
