d042e115ca58fb955b4dc4435acb5c493c6a82b0
aminet2a <aminetbo@gmail.com>
2023-02-06 22:06:08 +0100

    add methods implementations to Customer Service

--- src/main/java/patika/dev/definex/service/CustomerService.java
+++ src/main/java/patika/dev/definex/service/CustomerService.java
@@ -1,25 +1,34 @@
 package patika.dev.definex.service;
 
 import patika.dev.definex.entity.Customer;
+import patika.dev.definex.mockData.MockData;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CustomerService {
 
-    public List<Customer> getCustomerList() {
-        return null;
+    public List<Customer> getCustomerList() throws IOException {
+        return MockData.getCustomers();
     }
 
-    public void addCustomer(Customer customer) {
-
+    public void addCustomer(Customer customer) throws IOException {
+        MockData.getCustomers().add(customer);
     }
 
-    public List<Customer> filterCustomerByName(String subName) {
-        return null;
+    public List<Customer> filterCustomerByName(String subName) throws IOException {
+        return MockData.getCustomers()
+                .parallelStream()
+                .filter(customer -> customer.getName().contains(subName))
+                .collect(Collectors.toList());
     }
 
-    public List<Customer> getCustomerByInsertionMonth(int month) {
-        return null;
+    public List<Customer> getCustomerByInsertionMonth(int month) throws IOException {
+        return MockData.getCustomers()
+                .parallelStream()
+                .filter(customer -> customer.getInsDate().getMonthValue() == month)
+                .collect(Collectors.toList());
     }
 
 }
