a462c4737fb89e8ffb333304c0caf37d541829ce
aminet2a <aminetbo@gmail.com>
2023-02-06 21:36:03 +0100

    create Invoice Service class with initial needed methods.

---
+++ src/main/java/patika/dev/definex/service/InvoiceService.java
@@ -0,0 +1,34 @@
+package patika.dev.definex.service;
+
+import patika.dev.definex.entity.Invoice;
+
+import java.util.List;
+
+
+public class InvoiceService {
+
+    public List<Invoice> getAllInvoices() {
+        return null;
+    }
+
+    public List<Invoice> getTotalInvoicesByMonth(int month) {
+        return null;
+    }
+
+    public List<String> getTotalInvoicesByMonth(int month, double average) {
+        return null;
+    }
+
+    public List<Invoice> getInvoicesTotalGreaterThan(double total) {
+        return null;
+    }
+
+    public List<String> getInvoicesTotalSmallerThanCustomerName(double total) {
+        return null;
+    }
+
+    public void getInvoicesAverageOfTotalAmountGreaterThan(double total) {
+
+    }
+
+}
