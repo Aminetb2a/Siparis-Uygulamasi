d30157253544ec553406904c2f6af5ad2e7d3dc4
aminet2a <aminetbo@gmail.com>
2023-02-12 19:39:29 +0100

    add display updates

--- src/main/java/patika/dev/definex/SiparisUygulamasiApplication.java
+++ src/main/java/patika/dev/definex/SiparisUygulamasiApplication.java
@@ -1,15 +1,21 @@
 package patika.dev.definex;
 
+import com.github.lalyos.jfiglet.FigletFont;
 import patika.dev.definex.entity.Customer;
 import patika.dev.definex.service.CommonService;
 import patika.dev.definex.service.CustomerService;
 import patika.dev.definex.service.InvoiceService;
 
 import java.io.IOException;
+import java.time.LocalDate;
 
 public class SiparisUygulamasiApplication {
 
+    private final static String leftAlignFormat = "%n%n\t************************* %s *************************%n%n";
+
     public static void main(String[] args) throws IOException {
+
+        System.out.println(FigletFont.convertOneLine("Sipariş Uygulaması"));
 
         InvoiceService invoiceService = new InvoiceService();
         CustomerService customerService = new CustomerService();
@@ -17,58 +23,59 @@ public class SiparisUygulamasiApplication {
 
 
         //Tüm müşterileri listeleyin
-        System.out.println("\n\n*************************Tüm müşterileri listeleyin*************************\n");
+        System.out.format(leftAlignFormat, "Tüm müşterileri listesı");
         customerService.getAllCustomer().stream().forEach(System.out::println);
 
 
-        //        Yeni Müşteri oluşturabilen
+        //Yeni Müşteri oluşturabilen
         Customer customer = Customer.builder()
-                .customerNo(999)
                 .id(42857)
                 .name("Tandy")
-                .phone("+7 871 482 3332")
-                .insDate("01/09/2022")
+                .customerNo(436)
                 .surname("Niessen")
-                .customerNo(43)
                 .postalCode("5504")
-                .shipAddress("474 Cordelia Terrace")
+                .phone("+7 871 482 3332")
+                .email("tandy4@paypal.com")
                 .billAddress("461 Melrose Terrace")
+                .shipAddress("474 Cordelia Terrace")
+                .insDate(LocalDate.now().toString())
                 .build();
         customerService.addCustomer(customer);
 
 
-        //        İçerisinde ‘C’ harfi olan müşterileri listeleyin
-        System.out.println("\n\n*************************İçerisinde ‘C’ harfi olan müşterileri listeleyin*************************\n");
-        System.out.println((customerService.getCustomerByName("c")));
+        //İçerisinde ‘C’ harfi olan müşterileri listeleyin
+        System.out.format(leftAlignFormat, "İçerisinde ‘C’ harfi olan müşterileri listesı");
+        customerService.getCustomerByName("c").stream().forEach(System.out::println);
 
 
-        //        Haziran ayında kayıt olan müşterilerin faturalarınının toplam tutarını listeleyin
-        System.out.println("\n\n*************************Haziran ayında kayıt olan müşterilerin faturalarınının toplam tutarını listeleyin*************************\n");
-        System.out.println((commonService.getTotalAmountOfAllCustomersByInsertionMonth(06)));
+        //Haziran ayında kayıt olan müşterilerin faturalarınının toplam tutarını listeleyin
+        System.out.format(leftAlignFormat, "Haziran ayında kayıt olan müşterilerin faturalarınının toplam tutarını listesı");
+        System.out.println((commonService.getTotalAmountOfAllCustomersByInsertionMonth(6)));
 
 
-        //        Sistemdeki tüm faturaları listeleyin
-        System.out.println("\n\n*************************Sistemdeki tüm faturaları listeleyin*************************\n");
+        //Sistemdeki tüm faturaları listeleyin
+        System.out.format(leftAlignFormat, "Sistemdeki tüm faturaları listesı");
         invoiceService.getAllInvoices().stream().forEach(System.out::println);
 
 
-        //        Sistemdeki 1500TL üstündeki faturaları listeleyin
-        System.out.println("\n\n*************************Sistemdeki 1500TL üstündeki faturaları listeleyin*************************\n");
+        //Sistemdeki 1500TL üstündeki faturaları listeleyin
+        System.out.format(leftAlignFormat, "Sistemdeki 1500TL üstündeki faturaları listesı");
         invoiceService.getInvoicesTotalGreaterThan(1500).stream().forEach(System.out::println);
 
 
-        //        Sistemdeki 1500TL üstündeki faturaları ortalamasını hesaplayın
-        System.out.println("\n\n*************************Sistemdeki 1500TL üstündeki faturaları ortalamasını hesaplayın*************************\n");
+        //Sistemdeki 1500TL üstündeki faturaları ortalamasını hesaplayın
+        System.out.format(leftAlignFormat, "Sistemdeki 1500TL üstündeki faturaları ortalaması");
         System.out.println((invoiceService.getAverageOfInvoicesTotalAmountGreaterThan(1500)));
 
 
-        //        Sistemdeki 500TL altındaki faturalara sahip müşterilerin isimleri listeleyin
-        System.out.println("\n\n*************************Sistemdeki 500TL altındaki faturalara sahip müşterilerin isimleri listeleyin*************************\n");
+        //Sistemdeki 500TL altındaki faturalara sahip müşterilerin isimleri listeleyin
+        System.out.format(leftAlignFormat, "Sistemdeki 500TL altındaki faturalara sahip müşterilerin isimleri listesı");
         invoiceService.getCustomerNamesWithTotalInvoicesSmallerThan(500).stream().forEach(System.out::println);
 
 
-        //        Haziran ayını faturalarını ortalaması 750 altı olan firmalarının hangi sektörde olduğunu listeleyen kodu yazın.
-        System.out.println("\n\n*************************Haziran ayını faturalarını ortalaması 750 altı olan firmalarının hangi sektörde olduğunu listeleyen kodu yazın.*************************\n");
-        invoiceService.getTotalAmountAverageInvoicesByMonth(06, 750).stream().forEach(System.out::println);
+        //Haziran ayını faturalarını ortalaması 750 altı olan firmalarının hangi sektörde olduğunu listeleyen kodu yazın.
+        System.out.format(leftAlignFormat, "Haziran ayını faturalarını ortalaması 750 altı olan firmalarının sektör listesı");
+        invoiceService.getTotalAmountAverageInvoicesByMonth(6, 750).stream().forEach(System.out::println);
+
     }
 }
